2bff879da633e1e876a51bc731aee2dde8cfbd05
George Balota <george99.bl@gmail.com>
2021-10-08 20:13:47 +0300

    Initial commit

---
+++ HW1/t1.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+
+using namespace std;
+
+// Area and perimeter
+
+//int main() {
+//	int radius;
+//	cout << "Circle radius :";
+//	cin >> radius;
+//	cout << "Area: ";
+//	cout << 3.14159 * radius * radius;
+//	cout << "Perimeter: ";
+//	cout << 2 * 3.14159 * radius;
+//	return 0;
+//}
+
+// GCD
+
+//#include<iostream>
+//
+//using namespace std;
+//
+//int main() {
+//	int a, b;
+//	cin >> a;
+//	cin >> b;
+//	while (a != b) {
+//		if (a > b)
+//			a = a - b;
+//		else
+//			b = b - a;
+//	}
+//	cout << a;
+//	return 0;
+//}
+
+
+// Sum of n numbers
+
+
+//int main() {
+//	int n, s, i, e;
+//	cin >> n;
+//	s = 0;
+//	i = 0;
+//	while (i < n) {
+//		cin >> e;
+//		i = i + 1;
+//		s = s + e;
+//	}
+//	cout << s;
+//	return 0;
+//}
+
+
+// Just MLP errors
+
+//#include<iostream>
+//
+//using namespace std;
+//
+//int main() {
+//	int a, b;
+//	cin >> a >> b;
+//	cout << "Suma: " << a + b << " Diferenta: " << a - b;
+//	return 0;
+//}
+
+// Error in C++ syntax and MLP definition.
+// 
+//int main() {
+//	int n, i;
+//	cin >> n
+//		while (i < n)
+//			i++
+//			return 0;
+//}
